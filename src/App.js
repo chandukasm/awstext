77201ad341732baf99cc016b6ea458855d56271a
chanduka <ashainxsamarasinghe@gmail.com>
2020-12-11 23:34:02 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Amplify from "aws-amplify";
+import {
+  AmplifyAuthenticator,
+  AmplifySignOut,
+  AmplifySignIn,
+  AmplifySignUp,
+} from "@aws-amplify/ui-react";
+// import { withAuthenticator } from "@aws-amplify/ui-react";
+// import { Authenticator } from "aws-amplify-react/dist/Auth";
+// import { SignIn } from "aws-amplify-react";
+import awsconfig from "./aws-exports";
+import CustomSignIn from "./CustomSignIn";
+Amplify.configure(awsconfig);
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AmplifyAuthenticator>
+      <AmplifySignIn headerText="My Custom Sign In Header" slot="sign-in" />
+      <AmplifySignUp headerText="My Customer Sign Up Header" slot="sign-up" />
+
+      <div>
+        My App
+        <AmplifySignOut />
+      </div>
+    </AmplifyAuthenticator>
   );
-}
+};
 
 export default App;
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <Authenticator hide={[SignIn]} amplifyConfig={awsconfig}>
+//           <CustomSignIn />
+//           <App />
+//         </Authenticator>
+//       </div>
+//     );
+//   }
+// }
+// export default App;
+// export default withAuthenticator(App, true);
